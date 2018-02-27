6da6143a5b9b0348e30b080e53a672044610ff54
Juan Carlos Sánchez G <jcarlos0305@hotmail.com>
2018-02-27 14:21:36 -0600

    Deploy feature

--- build.js
+++ build.js
@@ -1,15 +1,34 @@
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 const pug = require('pug');
 const yamlFront = require('yaml-front-matter');
 const beautifyHtml = require('js-beautify').html;
 
-const fileTree = fs.readdirSync('./content');
+fsExtra.ensureDirSync('./dist');
+fsExtra.ensureDirSync('./dist/static/styles');
+fsExtra.ensureDirSync('./dist/static/img');
+
+try {
+  fsExtra.copySync('./public/styles/main.css', './dist/static/styles/main.css');
+  fsExtra.copySync('./public/styles/example.css', './dist/static/styles/example.css');
+  const images = fs.readdirSync('./public/img');
+  images.forEach((img) => {
+    fsExtra.copySync(`./public/img/${img}`, `./dist/static/img/${img}`);
+  });
+} catch (err) {
+  console.error(err);
+}
+const fileTree = fs.readdirSync('./views');
 
 fileTree.forEach((file) => {
-  file = file.slice(0, -3);
-  const frontMatter = yamlFront.loadFront(`./content/${file}.md`);
-  const compiledFunction = pug.compileFile(`views/${file}.pug`);
-  const html = compiledFunction(frontMatter);
-
-  fs.writeFileSync(`./dist/${file}.html`, beautifyHtml(html, { indent_size: 4 }));
+  if (file.substr(file.length - 4) === '.pug') {
+    const filename = file.slice(0, -4);
+    let frontMatter = {};
+    if (fs.existsSync(`./content/${filename}.md`)) {
+      frontMatter = yamlFront.loadFront(`./content/${filename}.md`);
+    }
+    const compiledFunction = pug.compileFile(`views/${filename}.pug`);
+    const html = compiledFunction(frontMatter);
+    fs.writeFileSync(`./dist/${filename}.html`, beautifyHtml(html, { indent_size: 4 }));
+  }
 });
