84a9826fa82c18b4238fcb707add18da65204372
Juan Carlos Sánchez G <jcarlos0305@hotmail.com>
2018-03-01 20:26:26 -0600

    Partials implemented

--- new-view.js
+++ new-view.js
@@ -3,4 +3,12 @@ const fs = require('fs');
 
 const [,, ...args] = process.argv;
 
-fs.writeFileSync(`./views/${args[0]}.pug`, `extends shared/layout\n\nblock content\n\th1 ${args[0]}`);
+console.log(args.length);
+
+if (args.length === 3) {
+  console.log(`using ${args[2]} template`);
+  fs.writeFileSync(`./views/${args[0]}.pug`, `extends layouts/${args[2]}\n\nblock content\n\th1 ${args[0]}`);
+} else {
+  fs.writeFileSync(`./views/${args[0]}.pug`, `block content\n\th1 ${args[0]}`);
+}
+
